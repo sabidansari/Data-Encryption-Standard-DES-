d8330759e44e062af4d29ede43cc00913c018bf0
sabidansari <62107795+sabidansari@users.noreply.github.com>
2020-03-12 20:13:31 +0530

    Add files via upload

---
+++ des.java
@@ -0,0 +1,199 @@
+package demo;
+public class des {
+    String[] bindigits = {"0000","0001","0010","0011","0100","0101","0110","0111","1000","1001","1010","1011","1100","1101","1110","1111"};
+    String[] hexdigits={"0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F",};
+    int[] initialpermutation={58,50,42,34,26,18,10,2,60,52,44,36,28,20,12,4,62,54,46,38,30,22,14,6,64,56,48,40,32,24,16,8,
+                               57,49,41,33,25,17,9,1,59,51,43,35,27,19,11,3,61,53,45,37,29,21,13,5,63,55,47,39,31,23,15,7};
+    int[] finalpermutation={40,8,48,16,56,24,64,32,39,7,47,15,55,23,63,31,38,6,46,14,54,22,62,30,37,5,45,13,53,21,61,29,
+                            36,4,44,12,52,20,60,28,35,3,43,11,51,19,59,27,34,2,42,10,50,18,58,26,33,1,41,9,49,17,57,25};
+    int[] paritydrop={57,49,41,33,25,17,9,1,58,50,42,34,26,18,10,2,59,51,43,35,27,19,11,3,60,52,44,36,63,55,47,39,31,23,
+                      15,7,62,54,46,38,30,22,14,6,61,53,45,37,29,21,13,5,28,20,12,4};
+    int[] keycompression={14,17,11,24,1,5,3,28,15,6,21,10,23,19,12,4,26,8,16,7,27,20,13,2,41,52,31,37,47,55,30,40,51,45,33,
+                          48,44,49,39,56,34,53,46,42,50,36,29,32};
+    int[] expansionP={32,1,2,3,4,5,4,5,6,7,8,9,8,9,10,11,12,13,12,13,14,15,16,17,16,17,18,19,20,21,20,21,22,23,24,25,24,25,26,27,28,
+                        29,28,29,30,31,32,1};
+    int[] straightP={16,7,20,21,29,12,28,17,1,15,23,26,5,18,31,10,2,8,24,14,32,27,3,9,19,13,30,6,22,11,4,25};
+    int[] shiftNo={1,1,2,2,2,2,2,2,1,2,2,2,2,2,2,1};
+    
+    int[][][] sbox = {{{14,4,13,1,2,15,11,8,3,10,6,12,5,9,0,7},{0,15,7,4,14,2,13,1,10,6,12,11,9,5,3,8},
+			{4,1,14,8,13,6,2,11,15,12,9,7,3,10,5,0},{15,12,8,2,4,9,1,7,5,11,3,14,10,0,6,13}},
+			{{15,1,8,14,6,11,3,4,9,7,2,13,12,0,5,10},{3,13,4,7,15,2,8,14,12,0,1,10,6,9,11,5},
+			{0,14,7,11,10,4,13,1,5,8,12,6,9,3,2,15},{13,8,10,1,3,15,4,2,11,6,7,12,0,5,14,9}},
+			{{10,0,9,14,6,3,15,5,1,13,12,7,11,4,2,8},{13,7,0,9,3,4,6,10,2,8,5,14,12,11,15,1},
+			{13,6,4,9,8,15,3,0,11,1,2,12,5,10,14,7},{1,10,13,0,6,9,8,7,4,15,14,3,11,5,2,12}},
+			{{7,13,14,3,0,6,9,10,1,2,8,5,11,12,4,15},{13,8,11,5,6,15,0,3,4,7,2,12,1,10,14,9},
+			{10,6,9,0,12,11,7,13,15,1,3,14,5,2,8,4},{3,15,0,6,10,1,13,8,9,4,5,11,12,7,2,14}},
+			{{2,12,4,1,7,10,11,6,8,5,3,15,13,0,14,9},{14,11,2,12,4,7,13,1,5,0,15,10,3,9,8,6},
+			{4,2,1,11,10,13,7,8,15,9,12,5,6,3,0,14},{11,8,12,7,1,14,2,13,6,15,0,9,10,4,5,3 }},
+			{{12,1,10,15,9,2,6,8,0,13,3,4,14,7,5,11},{10,15,4,2,7,12,9,5,6,1,13,14,0,11,3,8},
+			{9,14,15,5,2,8,12,3,7,0,4,10,1,13,11,6},{4,3,2,12,9,5,15,10,11,14,1,7,6,0,8,13}},
+			{{ 4,11,2,14,15,0,8,13,3,12,9,7,5,10,6,1},{13,0,11,7,4,9,1,10,14,3,5,12,2,15,8,6},
+			{1,4,11,13,12,3,7,14,10,15,6,8,0,5,9,2},{6,11,13,8,1,4,10,7,9,5,0,15,14,2,3,12}},
+			{{ 13,2,8,4,6,15,11,1,10,9,3,14,5,0,12,7},{1,15,13,8,10,3,7,4,12,5,6,11,0,14,9,2},
+			{7,11,4,1,9,12,14,2,0,6,10,13,15,3,5,8},{2,1,14,7,4,10,8,13,15,12,9,0,3,5,6,11}}};
+    public String strToBin(String text)
+    {
+        String temp="";
+        for(int i=0;i<text.length();i++)
+	{
+            int t=Integer.parseInt(Character.toString(text.charAt(i)), 16);
+            temp=temp+bindigits[t];
+	}return temp;
+    }
+    public String binToStr(String text)
+    {
+        String temp="";
+        for(int i=0;i<text.length();i=i+4)
+	{
+            temp=temp+hexdigits[Integer.parseInt(text.substring(i,i+4),2)];
+	}return temp;
+    }
+    public String permute(String s,int[] p,int n)
+    {
+        String temp="";
+        for(int i=0;i<n;i++)
+        {
+            temp=temp+s.charAt(p[i]-1);
+        }return temp;
+    }
+    String shiftLeft(String k, int n)
+    {
+        String temp="";
+        if(n==1)
+        {
+            for(int i=1;i<k.length();i++)
+            {
+                temp=temp+Character.toString(k.charAt(i));
+            }
+            temp=temp+Character.toString(k.charAt(0));
+        }
+        else if(n==2)
+        {
+            for(int i=2;i<k.length();i++)
+            {
+                temp=temp+Character.toString(k.charAt(i));
+            }
+            temp=temp+k.substring(0, 2);
+        }
+        return temp;
+    }
+    String[] generateKey(String key)
+    {
+        String[] temp=new String[16];
+        for(int i=0;i<16;i++)
+        {
+            String k0=key.substring(0, key.length()/2);
+            String k1=key.substring(key.length()/2);
+            k0=shiftLeft(k0,shiftNo[i]);
+            k1=shiftLeft(k1,shiftNo[i]);
+            String c=k0+k1;
+            temp[i]=permute(c,keycompression,keycompression.length);
+        }
+        return temp;
+    }
+    String XORED(String a,String b, int n)
+    {
+        String temp="";
+        for(int i=0;i<n;i++)
+        {
+            int r=Integer.parseInt(Character.toString(a.charAt(i)))^Integer.parseInt(Character.toString(b.charAt(i)));
+            temp=temp+r;
+        }
+        return temp;
+    }
+    String substitution(String text)
+    {
+        String temp="";
+        for(int i=0;i<text.length();i=i+6)
+        {
+            String t=text.substring(i,i+6);
+            int n=i/6;
+            int r=Integer.parseInt(t.charAt(0) + "" + t.charAt(5),2);
+            int c=Integer.parseInt(t.substring(1,5),2);
+            temp=temp+strToBin(Integer.toString(sbox[n][r][c]));
+        }
+        return temp;
+    }
+    String fiestalfunc(String text,String key)
+    {
+        text=permute(text,expansionP,expansionP.length);
+        String r=XORED(text,key,text.length());
+        r=substitution(r);
+        r=permute(r,straightP,straightP.length);
+        return r;
+    }
+    public String encrypt(String plaintext,String key)
+    {
+        String temp;
+        plaintext=strToBin(plaintext);
+        plaintext=permute(plaintext,initialpermutation,initialpermutation.length);
+        String l0=plaintext.substring(0, plaintext.length()/2);
+        String l1=plaintext.substring(plaintext.length()/2);
+        //Key generation
+        key=strToBin(key);
+        key=permute(key,paritydrop,paritydrop.length);
+        String[] keystore=generateKey(key);
+        
+        for(int i=0;i<16;i++)
+        {
+            if(i!=15)
+            {
+                String l11=fiestalfunc(l1,keystore[i]);
+                l0=XORED(l0,l11,l0.length());
+                String tt=l0;
+                l0=l1;
+                l1=tt;
+            }
+            else
+            {
+                String l11=fiestalfunc(l1,keystore[i]);
+                l0=XORED(l0,l11,l0.length());
+            }
+        }
+        String cc=l0+l1;
+        temp=permute(cc,finalpermutation,finalpermutation.length);
+        temp=binToStr(temp);
+        return temp;
+    }
+    public String decrypt(String plaintext,String key)
+    {
+        String temp;
+        plaintext=strToBin(plaintext);
+        plaintext=permute(plaintext,initialpermutation,initialpermutation.length);
+        String l0=plaintext.substring(0, plaintext.length()/2);
+        String l1=plaintext.substring(plaintext.length()/2);
+        //Key generation
+        key=strToBin(key);
+        key=permute(key,paritydrop,paritydrop.length);
+        String[] keystore=generateKey(key);
+        for (int i=15; i>-1;i--)
+        {
+            if(i!=0)
+            {
+                String l11=fiestalfunc(l1,keystore[i]);
+                l0=XORED(l0,l11,l0.length());
+                String tt=l0;
+                l0=l1;
+                l1=tt;
+            }
+            else
+            {
+                String l11=fiestalfunc(l1,keystore[i]);
+                l0=XORED(l0,l11,l0.length());
+            }
+        }
+        String cc=l0+l1;
+        temp=permute(cc,finalpermutation,finalpermutation.length);
+        temp=binToStr(temp);
+        return temp;
+    }
+    public static void main(String...des) {
+        des d=new des();
+        String plaintext="123456ABCD132536";
+        String key="AABB09182736CCDD";
+        String ciphertext=d.encrypt(plaintext,key);
+        System.out.println("PlainText:\t"+plaintext);
+        System.out.println("CipherText:\t"+ciphertext);
+        System.out.println("PlainText:\t"+d.decrypt(ciphertext,key));
+    }
+}
